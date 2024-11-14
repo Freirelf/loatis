65ace2abd657002fcba16c0cac7227fabb53a212
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-11-14 12:59:48 -0300

    implement Bottom Tabs

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Loatis!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" > Go to Profile</Link>
+      <Link href="/home" > Go to Profile</Link>
     </View>
   );
 }

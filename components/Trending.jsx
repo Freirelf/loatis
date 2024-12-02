ac277cc5f44c85eb2e3116e2430769acec38f0c4
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-12-02 15:45:47 -0300

    video adjustments

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
-import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
-import * as Animatable from 'react-native-animatable'
-import { icons } from '../constants'
-import { Video, ResizeMode } from 'expo-av'
+import { useState } from "react";
+import {
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
+import { VideoView, useVideoPlayer } from "expo-video";
 
 const zoomIn = {
   0: {
@@ -11,7 +16,7 @@ const zoomIn = {
   1: {
     scale: 1,
   },
-}
+};
 const zoomOut = {
   0: {
     scale: 1,
@@ -19,74 +24,81 @@ const zoomOut = {
   1: {
     scale: 0.8,
   },
-}
+};
 const TrendingItem = ({ activeItem, item }) => {
-  const [play, setPlay] = useState(false)
+  const [play, setPlay] = useState(false);
+
+  const videoSource = item.video;
+
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+  });
+
+  const handlePlayPause = () => {
+    if (play) {
+      player.pause();
+      setPlay(false);
+    } else {
+      player.play();
+      setPlay(true);
+    }
+  };
 
   return (
     <Animatable.View
       className="mr-5"
       animation={activeItem === item.$id ? zoomIn : zoomOut}
       duration={500}
-    > 
-    {play ? (
-      <Video 
-        source={{ uri: item.video }}
-        className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
-        resizeMode={ResizeMode.CONTAIN}
-        shouldPlay
-        onPlaybackStatusUpdate={(status) => {
-          if (status.didJustFinish) {
-            setPlay(false)
-          }
-        }}
-      />
-    ): (
-      <TouchableOpacity 
-        className="relative justify-center items-center"
-        onPress={() => setPlay(true)}
-      >
-        <ImageBackground 
-          source={{ uri: item.thumbnail }}
-          className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
-          resizeMode='cover'
+    >
+      {play ? (
+        <VideoView
+          player={player}
+          className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+          onPlaybackEnd={() => setPlay(false)}
         />
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          onPress={handlePlayPause}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
 
-        <Image 
-          source={icons.play}
-          className="w-12 h-12 absolute"
-          resizeMode='contain'
-        />
-
-      </TouchableOpacity>
-    )}
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
     </Animatable.View>
-  )
-}
+  );
+};
 const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[1])
+  const [activeItem, setActiveItem] = useState(posts[1]);
 
   const viewableItemsChanged = ({ viewableItems }) => {
     if (viewableItems.length > 0) {
-      setActiveItem(viewableItems[0].key)
+      setActiveItem(viewableItems[0].key);
     }
-  }
+  };
 
   return (
-    <FlatList 
+    <FlatList
       data={posts}
-      // key={item => item.$id}
       keyExtractor={(item) => item.$id}
-      renderItem={({ item}) => (
-        <TrendingItem activeItem={activeItem} item={item}/>
+      renderItem={({ item }) => (
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       onViewableItemsChanged={viewableItemsChanged}
       viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
       contentOffset={{ x: 170 }}
       horizontal
-      
     />
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,18 +1,39 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { VideoView, useVideoPlayer } from "expo-video";
 
-import { icons } from '../constants'
-import { Video, ResizeMode } from 'expo-av'
+import { icons } from "../constants";
 
-const VideoCard = ({video: { video, thumbnail, title, creator: {username, avatar }} }) => {
-  const [play, setPlay] = useState(false)
+const VideoCard = ({
+  video: {
+    video,
+    thumbnail,
+    title,
+    creator: { username, avatar },
+  },
+}) => {
+  const [play, setPlay] = useState(false);
+
+  const player = useVideoPlayer(video, (player) => {
+    player.loop = true;
+  });
+
+  const handlePlayPause = () => {
+    if (play) {
+      player.pause();
+      setPlay(false);
+    } else {
+      player.play();
+      setPlay(true);
+    }
+  };
 
   return (
     <View className="flex-col items-center px-4 mb-14">
       <View className="flex-row gap-3 items-start">
         <View className="justify-center items-center flex-row flex-1">
           <View className="w-[46px] h-[46px] rounded-lg border border-secondary p-0.5 items-center justify-center">
-            <Image 
+            <Image
               source={{ uri: avatar }}
               className="w-full h-full rounded-lg"
               resizeMode="cover"
@@ -20,44 +41,43 @@ const VideoCard = ({video: { video, thumbnail, title, creator: {username, avatar
           </View>
 
           <View className="justify-center flex-1 ml-3 gap-y-1">
-            <Text className="text-sm text-white font-psemibold" numberOfLines={1}>{title}</Text>
-            <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>{username}</Text>
-
+            <Text
+              className="text-sm text-white font-psemibold"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text
+              className="text-xs text-gray-100 font-pregular"
+              numberOfLines={1}
+            >
+              {username}
+            </Text>
           </View>
         </View>
         <View className="pt-2">
-          <Image 
-            source={icons.menu}
-            className="w-5 h-5"
-            resizeMode="contain"
-          />
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
         </View>
       </View>
 
       {play ? (
-        <Video 
-        source={{ uri: video }}
-        className="w-full h-60 rounded-xl mt-3"
-        resizeMode={ResizeMode.CONTAIN}
-        shouldPlay
-        onPlaybackStatusUpdate={(status) => {
-          if (status.didJustFinish) {
-            setPlay(false)
-          }
-        }}
-      />
-      ): (
-        <TouchableOpacity 
+        <VideoView
+          player={player}
+          className="w-full h-60 rounded-xl mt-3"
+          onPlaybackEnd={() => setPlay(false)} // Desativa a reprodução ao terminar
+        />
+      ) : (
+        <TouchableOpacity
           className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
           activeOpacity={0.7}
-          onPress={() => setPlay(true)}
-          >
-          <Image 
+          onPress={handlePlayPause}
+        >
+          <Image
             source={{ uri: thumbnail }}
             className="w-full h-full rounded-xl mt-3"
             resizeMode="cover"
           />
-           <Image 
+          <Image
             source={icons.play}
             className="w-12 h-12 absolute"
             resizeMode="cover"
@@ -65,7 +85,7 @@ const VideoCard = ({video: { video, thumbnail, title, creator: {username, avatar
         </TouchableOpacity>
       )}
     </View>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;

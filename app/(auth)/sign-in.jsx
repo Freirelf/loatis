e085eeaf2b00599effe15aad247526d6c3e37e78
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-12-05 17:25:37 -0300

    implement Profile Screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,10 +5,13 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from "../../components/CustomButton";
-import { Link } from 'expo-router'
-import { signIn } from '../../lib/appwrite'
+import { Link, router } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [form, setForm] = useState({
     email: '',
@@ -26,7 +29,10 @@ const SignIn = () => {
     try {
       await signIn(form.email, form.password)
 
-      // set it to global state.. 
+      const result = await getCurrentUser()
+
+      setUser(result)
+      setIsLoggedIn(true)
 
       router.replace('/home')
     } catch (error) {
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,8 +8,11 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+
   const { data: posts, refetch } = useAppwrite(getAllPosts)
   const { data: latestPosts  } = useAppwrite(getLatestPosts)
   const [refreshing, setRefreshing] = useState(false)
@@ -24,7 +27,6 @@ const Home = () => {
     <SafeAreaView className="bg-primary h-full">
       <FlatList 
         data={posts}
-        // key={item => item.$id}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <VideoCard video={item} />
@@ -34,10 +36,10 @@ const Home = () => {
             <View className="justify-between flex-row items-start mb-6">
               <View > 
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Bem-vindo de volta
+                  Bem-vindo de volta, 
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Lucas Freire
+                  {user?.username}
                 </Text>
               </View>
 
